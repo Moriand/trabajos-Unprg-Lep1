00eb0d9540691a3805dc5db94ef074189b9af5fa
Moriand <32554375+Moriand@users.noreply.github.com>
2017-11-11 16:06:05 -0500

    pregunta8.vb

--- pregunta8.vb
+++ pregunta8.vb
@@ -7,21 +7,31 @@ Dim num,num1 As String
 			num = CStr(Console.ReadLine)
 			num1 = StrReverse(num)
 			
-			If num1 = num Then
+			If num = -1 Then
+				Reset
+				
+			ElseIf  num1 = num Then
+				Console.ForegroundColor = ConsoleColor.Magenta
 				Console.WriteLine("  si es capicua")
-			Else
+				Console.ResetColor
+			Elseif num1 <>num Then
 				Console.WriteLine("   no es capicua")
+			ElseIf num = "-1" Then
+				Console.ForegroundColor = ConsoleColor.Cyan
+				Console.WriteLine("   el programa va terminar por ingresar -1")
+				Console.ResetColor
 			End If
-			Console.ForegroundColor = ConsoleColor.Green
+			
 			Do
+				Console.ForegroundColor = ConsoleColor.Green
 				Console.WriteLine("desea intentar con otro numero?")
 				Console.WriteLine("     1 = si")
 				Console.WriteLine("     2 = no ")
 				Console.ResetColor
 				Console.ForegroundColor = ConsoleColor.Red
 				snd = Console.ReadLine()
-Console.ResetColor
-			Loop Until snd>=1 And snd<=2 
+		Console.ResetColor
+			Loop Until snd>=1 And snd<=2 Or num = -1 
 		
 		End While
 		
