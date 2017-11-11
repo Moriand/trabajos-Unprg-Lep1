6516bb596f9579b97704014e3c1564e1adc68610
Moriand <32554375+Moriand@users.noreply.github.com>
2017-11-10 22:21:10 -0500

    pregunta11.vb
    Crear un programa que permita ingresar el consumo de 20 clientes, teniendo en cuenta
    que por cada S/50 acumula un bono, el programa debe de realizar lo siguiente
    Ingresar consumo de cliente
    con bono
    Cantidad de clientes sin bono
    Mostrar el monto total del consumo

---
+++ pregunta11.vb
@@ -0,0 +1,36 @@
+Dim consumo,tconsumo As Double
+		Dim bono,sbono, fin As Integer
+		fin=0
+		bono=0
+		tconsumo=0
+		While fin<>2
+			For index=1 To 20 
+			Console.WriteLine("Ingresar tu consumo n°{0}",index)
+			consumo = CDbl(Console.Readline())
+			If consumo = 50 Then
+				bono +=1
+			Else
+				tconsumo +=consumo
+				sbono +=1
+				
+				
+			End If
+			Next
+			
+			Console.ForegroundColor = ConsoleColor.Red
+			Do
+				
+			    Console.writeLine("si desea terminar ingrese 2, de lo contrario ingrese 1")
+			    fin= Console.ReadLine()
+			    
+			Loop Until fin>=1 And fin<=2
+			Console.ResetColor
+		End While
+		Console.ForegroundColor= ConsoleColor.Yellow
+		Console.WriteLine("Cantidad de clientes con bono {0}",bono)
+		Console.ForegroundColor = ConsoleColor.Green
+		Console.WriteLine("cantidad de clientes sin bono {0} ",sbono)
+		Console.ResetColor
+		Console.ForegroundColor = ConsoleColor.DarkBlue
+		Console.WriteLine("Monto total {0} ", tconsumo)
+		Console.ReadKey(True)
